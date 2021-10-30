d8b79d5d6b03917460f3b2dc023e3c0a8172e6a0
Juan Henrique <lacorte07@hotmail.com>
2021-10-30 18:12:57 -0300

    Implementação  Repositorio Base.

---
+++ SGF.Data/Repository/RepositoryBase.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SGF.Data.Context;
+using SGF.Domain.Entities;
+using SGF.Domain.Interface.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SGF.Data.Repository
+{
+    public abstract class RepositoryBase<TEntity> : IRepositoryBase<TEntity> where TEntity : BaseEntity, new()
+    {
+        protected readonly SGFDbContext Context;
+        protected readonly DbSet<TEntity> DbSet;
+
+        public RepositoryBase(SGFDbContext context)
+        {
+            Context = context;
+            DbSet = context.Set<TEntity>();
+        }
+        public virtual async Task Adicionar(TEntity entidade)
+        {
+            DbSet.Add(entidade);
+            await SaveChanges();
+        }
+
+        public virtual async Task Atualizar(TEntity entidade)
+        {
+            DbSet.Update(entidade);
+            await SaveChanges();
+        }
+
+        public async Task<IEnumerable<TEntity>> BuscarPorExpressao(Expression<Func<TEntity, bool>> predicado)
+        {
+            return await DbSet.AsNoTracking().Where(predicado).ToListAsync();
+        }
+
+        public async Task<TEntity> ObterEntidadePorId(Guid Id)
+        {
+            return await DbSet.FindAsync(Id);
+        }
+
+        public async Task<List<TEntity>> ObterTodasEntidades()
+        {
+            return await DbSet.ToListAsync();
+        }
+
+        public async Task Remover(Guid id)
+        {
+            DbSet.Remove(new TEntity { Id = id });
+            await SaveChanges();
+        }
+
+        public async Task<int> SaveChanges()
+        {
+            return await Context.SaveChangesAsync();
+        }
+
+        public void Dispose()
+        {
+            Context?.Dispose();
+        }
+    }
+}
--- SGF.Domain/Interface/Repository/IRepositoryBase.cs
+++ SGF.Domain/Interface/Repository/IRepositoryBase.cs
@@ -1,10 +1,21 @@
-﻿using System;
+﻿using SGF.Domain.Entities;
+using System;
 using System.Collections.Generic;
+using System.Linq.Expressions;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace SGF.Domain.Interface.Repository
 {
-    public interface IRepositoryBase
+    public interface IRepositoryBase<TEntity> : IDisposable where TEntity : BaseEntity
     {
+        Task Adicionar(TEntity entidade);
+        Task<TEntity> ObterEntidadePorId(Guid Id);
+        Task<List<TEntity>> ObterTodasEntidades();
+        Task Atualizar(TEntity entidade);
+        Task Remover(Guid id);
+        Task<IEnumerable<TEntity>> BuscarPorExpressao(Expression<Func<TEntity, bool>> predicado);
+        Task<int> SaveChanges();
+
     }
 }
